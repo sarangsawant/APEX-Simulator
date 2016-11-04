604c94003d9037def53b6e35298bbfbc8359dad4
Sarang Sawant <Sarang Sawant@149.125.32.25>
2016-11-04 00:30:34 -0400

    Program flow changed, created seperate Main class.

---
+++ Source/src/com/binghamton/cs520/simulator/Instruction.java
@@ -0,0 +1,14 @@
+package com.binghamton.cs520.simulator;
+
+public class Instruction {
+	private String instructionType;
+	private String source1;
+	private String source2;
+	private String source3;
+	private String destination;
+	private String literal;
+	
+	public Instruction() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ Source/src/com/binghamton/cs520/simulator/MainClass.java
@@ -0,0 +1,9 @@
+package com.binghamton.cs520.simulator;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		SimulateAPEXPipeline simulate = new SimulateAPEXPipeline();
+		simulate.simulateProcessingCycles();
+	}
+}
