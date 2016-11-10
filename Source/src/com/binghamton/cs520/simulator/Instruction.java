f95d7686912769001f44ee039aa1303a3a7d6ec6
Sarang Sawant <Sarang Sawant@192.168.2.6>
2016-11-10 11:53:13 -0500

    Instruction class added with basic pipeline workflow.

--- Source/src/com/binghamton/cs520/simulator/Instruction.java
+++ Source/src/com/binghamton/cs520/simulator/Instruction.java
@@ -1,6 +1,7 @@
 package com.binghamton.cs520.simulator;
 
 public class Instruction {
+	private String instruction;
 	private String instructionType;
 	private String source1;
 	private String source2;
@@ -11,4 +12,61 @@ public class Instruction {
 	public Instruction() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public String getInstruction() {
+		return instruction;
+	}
+
+	public void setInstruction(String instruction) {
+		this.instruction = instruction;
+	}
+
+	public String getInstructionType() {
+		return instructionType;
+	}
+
+	public void setInstructionType(String instructionType) {
+		this.instructionType = instructionType;
+	}
+
+	public String getSource1() {
+		return source1;
+	}
+
+	public void setSource1(String source1) {
+		this.source1 = source1;
+	}
+
+	public String getSource2() {
+		return source2;
+	}
+
+	public void setSource2(String source2) {
+		this.source2 = source2;
+	}
+
+	public String getSource3() {
+		return source3;
+	}
+
+	public void setSource3(String source3) {
+		this.source3 = source3;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getLiteral() {
+		return literal;
+	}
+
+	public void setLiteral(String literal) {
+		this.literal = literal;
+	}
+	
 }
--- Source/src/com/binghamton/cs520/simulator/SimulateAPEXPipeline.java
+++ Source/src/com/binghamton/cs520/simulator/SimulateAPEXPipeline.java
@@ -5,15 +5,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.binghamton.cs520.constants.Tokens;
 
 public class SimulateAPEXPipeline {
-	private static Map<Integer, String> instructions = new HashMap<>();
+	private static Map<Integer, Instruction> instructions = new HashMap<>();
 	private static int programCnt = 4000;
-	
+	private static List<Instruction> fetchStageList = new ArrayList<>();
+	private static List<Instruction> decodeStageList = new ArrayList<>();
+	private static List<Instruction> executeStageList = new ArrayList<>();
+	private static List<Instruction> memoryStageList = new ArrayList<>();
+	private static List<Instruction> writeBackStageList = new ArrayList<>();
 	
 	private void populateInstructionsInMap(){
 		String fileName = "src/Input.txt";
@@ -27,7 +33,9 @@ public class SimulateAPEXPipeline {
 			try {
 				while((line = br.readLine()) != null){
 					String data[] = line.split(Tokens.COLON.getToken());
-					instructions.put(Integer.parseInt(data[0]), data[1]);
+					Instruction instruction = new Instruction();
+					instruction.setInstruction(data[1]);
+					instructions.put(Integer.parseInt(data[0]),instruction);
 				}
 				br.close();
 				fr.close();
@@ -39,30 +47,84 @@ public class SimulateAPEXPipeline {
 		}
 	}
 	
-	private void fetchInstruction(){
-			String instruction = instructions.get(programCnt);
-			programCnt = programCnt + 1;
-			decodeInstruction(instruction);
-		
+	private void fetchStageExecution(){
+		System.out.println("Inside fetchStageExecution method");
+		System.out.println(fetchStageList.get(0).getInstruction());
 	}
 	
-	private void decodeInstruction(String instruction){
-		
-		String data[] = instruction.split(Tokens.SPACE.getToken());
+	private void decodeStageExecution(){
+		System.out.println("Inside decodeStageExecution method");
+		System.out.println("Instuction in decode stage -->" + decodeStageList.get(0).getInstruction());
+		//String data[] = instruction.split(Tokens.SPACE.getToken());
 		
 	}
 	
 	private void functionalUnitALU(){
 		System.out.println("functionalUnitALU");
+		System.out.println("Inside execute stage--> "+ executeStageList.get(0).getInstruction());
 	}
 	
-	private void functionalUnitBranch(){
-		System.out.println("functionalUnitBranch");
+	private void memoryStageExecution(){
+		System.out.println("memoryStageExecution");
+		System.out.println("Inside memory stage--> "+ memoryStageList.get(0).getInstruction());
 	}
 	
-	public void simulateProcessingCycles(){
+	private void writeBackStageExecution(){
+		System.out.println("writeBackStageExecution");
+		System.out.println("Inside writeBack stage--> "+ writeBackStageList.get(0).getInstruction());
+	}
+	
+	public void simulateProcessingCycles(int totalSimulateCycles){
 		populateInstructionsInMap();
-		fetchInstruction();
+		
+		for(int i=0; i < totalSimulateCycles ; i++){
+			System.out.println("\nCYCLE --" + i);
+			if(fetchStageList.size() == 0){
+				fetchStageList.add(instructions.get(programCnt));
+				programCnt = programCnt + 1;
+				fetchStageExecution();
+				continue;
+			}else{
+				fetchStageList.add(instructions.get(programCnt));
+				programCnt = programCnt + 1;
+				decodeStageList.add(fetchStageList.get(0));
+				fetchStageList.remove(0);
+				fetchStageExecution();
+			}
+			
+			if(decodeStageList.size() == 1){
+				decodeStageExecution();
+				continue;
+			}else{
+				executeStageList.add(decodeStageList.get(0));
+				decodeStageList.remove(0);
+				decodeStageExecution();
+			}
+			
+			if(executeStageList.size() == 1){
+				functionalUnitALU();
+				continue;
+			}else{
+				memoryStageList.add(executeStageList.get(0));
+				executeStageList.remove(0);
+				functionalUnitALU();
+			}
+			
+			if(memoryStageList.size() == 1){
+				memoryStageExecution();
+				continue;
+			}else{
+				writeBackStageList.add(memoryStageList.get(0));
+				memoryStageList.remove(0);
+				memoryStageExecution();
+			}
+			
+			if(writeBackStageList.size() == 1){
+				writeBackStageExecution();
+				writeBackStageList.remove(0);
+				continue;
+			}
+		}
 	}
 	
 }
